586ba50f68898e287d0577234d1ff874e7337ce4
Rahul <rksethuram9@gmail.com>
2019-02-19 15:39:27 -0800

    Hit with prettier

--- src/components/sendCard.js
+++ src/components/sendCard.js
@@ -11,22 +11,21 @@ import Modal from "@material-ui/core/Modal";
 import QRScan from "./qrScan";
 import { emptyAddress } from "connext/dist/Utils";
 
-
 class PayCard extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
       paymentVal: {
         meta: {
-          purchaseId: "payment",
+          purchaseId: "payment"
           // memo: "",
         },
         payments: [
           {
-            recipient: emptyAddress.substr(0, 4) + '...',
+            recipient: emptyAddress.substr(0, 4) + "...",
             amount: {
-              amountToken: "0",
+              amountToken: "0"
             },
             type: "PT_CHANNEL"
           }
@@ -34,27 +33,27 @@ class PayCard extends Component {
       },
       addressError: null,
       balanceError: null,
-      scan: false,
+      scan: false
     };
 
     if (this.props.scanArgs.recipient && this.props.scanArgs.amount) {
       this.state = {
         paymentVal: {
           meta: {
-            purchaseId: "payment",
+            purchaseId: "payment"
             // memo: "",
           },
           payments: [
             {
               recipient: this.props.scanArgs.recipient,
               amount: {
-                amountToken: this.props.scanArgs.amount,
+                amountToken: this.props.scanArgs.amount
               },
               type: "PT_CHANNEL"
             }
           ]
-        },
-      }
+        }
+      };
     }
   }
 
@@ -63,42 +62,32 @@ class PayCard extends Component {
       oldState.paymentVal.payments[0].amount.amountToken = value;
       return oldState;
     });
-    console.log(
-      `Updated paymentVal: ${JSON.stringify(this.state.paymentVal, null, 2)}`
-    );
+    console.log(`Updated paymentVal: ${JSON.stringify(this.state.paymentVal, null, 2)}`);
   }
 
   async updateRecipientHandler(value) {
     await this.setState(oldState => {
       oldState.paymentVal.payments[0].recipient = value;
-      return oldState
+      return oldState;
     });
-    this.setState({scan: false})
-    console.log(
-      `Updated recipient: ${JSON.stringify(
-        this.state.paymentVal.payments[0].recipient,
-        null,
-        2
-      )}`
-    );
+    this.setState({ scan: false });
+    console.log(`Updated recipient: ${JSON.stringify(this.state.paymentVal.payments[0].recipient, null, 2)}`);
   }
 
   async paymentHandler() {
-    console.log(
-      `Submitting payment: ${JSON.stringify(this.state.paymentVal, null, 2)}`
-    );
-    this.setState({addressError: null, balanceError: null})
+    console.log(`Submitting payment: ${JSON.stringify(this.state.paymentVal, null, 2)}`);
+    this.setState({ addressError: null, balanceError: null });
     const { channelState, connext, web3 } = this.props;
 
     // if( Number(this.state.paymentVal.payments[0].amount.amountToken) <= Number(channelState.balanceTokenUser) &&
     //     Number(this.state.paymentVal.payments[0].amount.amountWei) <= Number(channelState.balanceWeiUser)
     // ) {
-      if(web3.utils.isAddress(this.state.paymentVal.payments[0].recipient)) {
-        let paymentRes = await connext.buy(this.state.paymentVal);
-        console.log(`Payment result: ${JSON.stringify(paymentRes, null, 2)}`);
-      } else {
-        this.setState({addressError: "Please choose a valid address"})
-      }
+    if (web3.utils.isAddress(this.state.paymentVal.payments[0].recipient)) {
+      let paymentRes = await connext.buy(this.state.paymentVal);
+      console.log(`Payment result: ${JSON.stringify(paymentRes, null, 2)}`);
+    } else {
+      this.setState({ addressError: "Please choose a valid address" });
+    }
     // } else {
     //   this.setState({balanceError: "Insufficient balance in channel"})
     // }
@@ -118,7 +107,7 @@ class PayCard extends Component {
       },
       icon: {
         width: "40px",
-        height: "40px",
+        height: "40px"
       },
       input: {
         width: "100%"
@@ -128,8 +117,8 @@ class PayCard extends Component {
         backgroundColor: "#FCA311",
         color: "#FFF",
         marginLeft: "5px",
-        marginRight: "5px",
-      },
+        marginRight: "5px"
+      }
     };
 
     return (
@@ -150,7 +139,7 @@ class PayCard extends Component {
           error={this.state.balanceError != null}
         />
         <TextField
-          style={{width: "100%"}}
+          style={{ width: "100%" }}
           id="outlined-with-placeholder"
           label="Recipient"
           placeholder="0x0... (Optional for Link)"
@@ -164,12 +153,7 @@ class PayCard extends Component {
             endAdornment: (
               <InputAdornment position="end">
                 <Tooltip disableFocusListener disableTouchListener title="Scan with QR code">
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    style={{color: "#FFF"}}
-                    onClick={()=> this.setState({scan: true})}
-                  >
+                  <Button variant="contained" color="primary" style={{ color: "#FFF" }} onClick={() => this.setState({ scan: true })}>
                     <QRIcon />
                   </Button>
                 </Tooltip>
@@ -177,15 +161,8 @@ class PayCard extends Component {
             )
           }}
         />
-        <Modal
-          id="qrscan"
-          open={this.state.scan}
-          onClose={() => this.setState({scan: false})}
-          style={{ width: "full", height: "full" }}
-        >
-          <QRScan
-            handleResult = {this.updateRecipientHandler.bind(this)} 
-          />
+        <Modal id="qrscan" open={this.state.scan} onClose={() => this.setState({ scan: false })} style={{ width: "full", height: "full" }}>
+          <QRScan handleResult={this.updateRecipientHandler.bind(this)} />
         </Modal>
         {/* <TextField
           style={cardStyle.input}
@@ -209,16 +186,11 @@ class PayCard extends Component {
             //TODO ENABLE THIS WHEN WE ADD FUNCTIONALITY
           >
             Link
-            <LinkIcon style={{marginLeft: "5px"}}/>
+            <LinkIcon style={{ marginLeft: "5px" }} />
           </Button>
-          <Button
-            style={cardStyle.button}
-            variant="contained"
-            size="large"
-            onClick={() => this.paymentHandler()}
-          >
+          <Button style={cardStyle.button} variant="contained" size="large" onClick={() => this.paymentHandler()}>
             Send
-            <SendIcon style={{marginLeft: "5px"}}/>
+            <SendIcon style={{ marginLeft: "5px" }} />
           </Button>
         </div>
       </Card>
