8d05c33d522dc995d933dd4ff00436d19d7117a8
Rahul <rksethuram9@gmail.com>
2019-02-19 15:36:46 -0800

    Fix receive card to be in grid

--- src/components/receiveCard.js
+++ src/components/receiveCard.js
@@ -6,14 +6,22 @@ import TextField from "@material-ui/core/TextField";
 import Tooltip from "@material-ui/core/Tooltip";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import Typography from "@material-ui/core/Typography";
-import CopyIcon from "@material-ui/icons/FileCopy"
-import QRGenerate from "./qrGenerate"
+import CopyIcon from "@material-ui/icons/FileCopy";
+import QRGenerate from "./qrGenerate";
+import { withStyles, Grid } from "@material-ui/core";
 const Web3 = require("web3");
 const eth = require("ethers");
 
+const styles = {
+  icon: {
+    width: "40px",
+    height: "40px"
+  }
+};
+
 class ReceiveCard extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
       value: "0",
@@ -23,82 +31,64 @@ class ReceiveCard extends Component {
   }
 
   async updatePaymentHandler(evt) {
-    const qrUrl = this.generateQrUrl(evt.target.value)
+    const qrUrl = this.generateQrUrl(evt.target.value);
     this.setState({
       value: evt.target.value,
-      qrUrl,
+      qrUrl
     });
-    console.log(
-      `Updated value: ${this.state.value}`
-    );
+    console.log(`Updated value: ${this.state.value}`);
   }
 
   generateQrUrl(value) {
+    const { publicUrl, address } = this.props
     // function should take a payment value
     // and convert it to the url with
     // appropriate strings to prefill a send
     // modal state (recipient, amountToken)
-    const url = `${this.props.publicUrl}?amountToken=${value}&recipient=${this.props.address}`
-    console.log('QR code url:', url)
-    return url
+    const url = `${publicUrl}?amountToken=${value}&recipient=${address}`;
+    console.log("QR code url:", url);
+    return url;
   }
 
   render() {
-    const cardStyle = {
-      card: {
-        display: "flex",
-        flexWrap: "wrap",
-        flexDirection: "row",
-        width: "100%",
-        height: "70%",
-        justifyContent: "center",
-        backgroundColor: "#FFFFFF",
-        padding: "4% 4% 4% 4%"
-      },
-      icon: {
-        width: "40px",
-        height: "40px",
-      },
-      input: {
-        width: "100%"
-      },
-    };
-
+    const { classes, address } = this.props;
+    const { qrUrl, error, displayVal } = this.state
     return (
-      <Card style={cardStyle.card}>
-        <ReceiveIcon style={cardStyle.icon} />
-        <TextField
-          style={cardStyle.input}
-          id="outlined-number"
-          label="Amount"
-          value={this.state.displayVal}
-          type="number"
-          margin="normal"
-          variant="outlined"
-          onChange={evt => this.updatePaymentHandler(evt)}
-          error={this.state.error != null}
-          helperText={this.state.error}
-        />
-        <QRGenerate
-          value={this.state.qrUrl}
-        />
-        <Button variant="outlined">
-          <CopyIcon style={{marginRight: "5px"}} />
-          <CopyToClipboard text={(this.props.address)}>
-            <Typography noWrap variant="body1">
-              <Tooltip
-                disableFocusListener
-                disableTouchListener
-                title="Click to Copy"
-              >
-                <span>{this.state.qrUrl}</span>
-              </Tooltip>
-            </Typography>
-          </CopyToClipboard>
-        </Button>
-      </Card>
+      <Grid container spacing={24} direction="column" alignItems="center" justify="center">
+        <Grid item xs={12}>
+          <ReceiveIcon className={classes.icon} />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            id="outlined-number"
+            label="Amount"
+            value={displayVal}
+            type="number"
+            margin="normal"
+            variant="outlined"
+            onChange={evt => this.updatePaymentHandler(evt)}
+            error={error != null}
+            helperText={error}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <QRGenerate value={qrUrl} />
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="outlined">
+            <CopyIcon style={{ marginRight: "5px" }} />
+            <CopyToClipboard text={address}>
+              <Typography noWrap variant="body1">
+                <Tooltip disableFocusListener disableTouchListener title="Click to Copy">
+                  <span>{qrUrl}</span>
+                </Tooltip>
+              </Typography>
+            </CopyToClipboard>
+          </Button>
+        </Grid>
+      </Grid>
     );
   }
 }
 
-export default ReceiveCard;
+export default withStyles(styles)(ReceiveCard);
