4ce3bb0547a3d545d364aa60cb0fb67489558ecb
Rahul <rksethuram9@gmail.com>
2019-02-19 14:16:49 -0800

    Extract app bar into app

--- src/components/depositCard.js
+++ src/components/depositCard.js
@@ -1,21 +1,23 @@
 import React, { Component } from "react";
-import Card from "@material-ui/core/Card";
 import Button from "@material-ui/core/Button";
 import DepositIcon from "@material-ui/icons/AttachMoney";
-import TextField from "@material-ui/core/TextField";
 import Tooltip from "@material-ui/core/Tooltip";
 import { CopyToClipboard } from "react-copy-to-clipboard";
-import Switch from "@material-ui/core/Switch";
-import HelpIcon from "@material-ui/icons/Help";
-import IconButton from "@material-ui/core/IconButton";
-import Popover from "@material-ui/core/Popover";
 import Typography from "@material-ui/core/Typography";
 import CopyIcon from "@material-ui/icons/FileCopy";
 import Grid from "@material-ui/core/Grid";
-import CardContent from "@material-ui/core/Grid";
 import QRGenerate from "./qrGenerate";
-const Web3 = require("web3");
-const eth = require("ethers");
+import { withStyles } from "@material-ui/core";
+
+const styles = theme => ({
+  paper: {
+    paddingBottom: theme.spacing.unit * 2
+  },
+  icon: {
+    width: "40px",
+    height: "40px"
+  }
+});
 
 class DepositCard extends Component {
   constructor(props) {
@@ -28,76 +30,38 @@ class DepositCard extends Component {
   }
 
   render() {
-    const cardStyle = {
-      card: {
-        display: "flex",
-        flexWrap: "wrap",
-        flexDirection: "row",
-        width: "100%",
-        height: "70%",
-        justifyContent: "center",
-        backgroundColor: "#FFFFFF",
-        padding: "4% 4% 4% 4%"
-      },
-      icon: {
-        width: "40px",
-        height: "40px"
-      },
-      input: {
-        width: "100%",
-        padding: "2% 2% 2% 2%"
-      },
-      row: {
-        width: "100%",
-        textAlign: "center",
-        padding: "2% 2% 2% 2%"
-      }
-    };
+    const { classes, address } = this.props;
 
     return (
-      <Card style={cardStyle.card}>
-        <CardContent>
-          <Grid container spacing={24} direction="column" alignItems="center">
-            <Grid item xs={12}>
-              <DepositIcon style={cardStyle.icon} />
-            </Grid>
-            <Grid item xs={12}>
-              <Typography variant="caption">
-                <Tooltip
-                  disableFocusListener
-                  disableTouchListener
-                  title="Because gas"
-                >
-                  <span>{`Deposit minimum ${
-                    this.props.minDepositWei
-                  } wei to card.`}</span>
+      <Grid container spacing={24} direction="column" alignItems="center">
+        <Grid item xs={12}>
+          <DepositIcon className={classes.icon} />
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="caption">
+            <Tooltip disableFocusListener disableTouchListener title="Because gas">
+              <span>{`Deposit minimum ${this.props.minDepositWei} wei to card.`}</span>
+            </Tooltip>
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <QRGenerate value={address} />
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="outlined">
+            <CopyIcon style={{ marginRight: "5px" }} />
+            <CopyToClipboard text={address}>
+              <Typography noWrap variant="body1">
+                <Tooltip disableFocusListener disableTouchListener title="Click to Copy">
+                  <span>{address}</span>
                 </Tooltip>
               </Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <QRGenerate value={this.props.address} />
-            </Grid>
-            <Grid item xs={12}>
-              <Button variant="outlined">
-                <CopyIcon style={{ marginRight: "5px" }} />
-                <CopyToClipboard text={this.props.address}>
-                  <Typography noWrap variant="body1">
-                    <Tooltip
-                      disableFocusListener
-                      disableTouchListener
-                      title="Click to Copy"
-                    >
-                      <span>{this.props.address}</span>
-                    </Tooltip>
-                  </Typography>
-                </CopyToClipboard>
-              </Button>
-            </Grid>
-          </Grid>
-        </CardContent>
-      </Card>
+            </CopyToClipboard>
+          </Button>
+        </Grid>
+      </Grid>
     );
   }
 }
 
-export default DepositCard;
+export default withStyles(styles)(DepositCard);
