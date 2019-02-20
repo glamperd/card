7f428f331464f27bd9705045fe27a8a0502e3132
Rahul <rksethuram9@gmail.com>
2019-02-19 17:18:00 -0800

    Add some responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,6 @@ import "../App.css";
 import CashOutCard from "./cashOutCard";
 import ChannelCard from "./channelCard";
 import QRScan from "./qrScan";
-import SettingsCard from "./settingsCard";
 import QRIcon from "mdi-material-ui/QrcodeScan";
 import SendIcon from "@material-ui/icons/Send";
 import ReceiveIcon from "@material-ui/icons/SaveAlt";
@@ -12,11 +11,7 @@ import Button from "@material-ui/core/Button";
 import { Fab, Grid, withStyles } from "@material-ui/core";
 import { Link } from 'react-router-dom'
 
-const styles = theme => ({
-  paper: {
-    paddingBottom: theme.spacing.unit * 2
-  }
-});
+const styles = {}
 
 class Home extends React.Component {
   state = {
@@ -45,7 +40,7 @@ class Home extends React.Component {
 
   render() {
     const { modals } = this.state;
-    const { classes, address, channelState, publicUrl } = this.props;
+    const { address, channelState } = this.props;
     return (
       <>
         <div className="row" style={{ marginBottom: "-7.5%" }}>
@@ -76,9 +71,9 @@ class Home extends React.Component {
           </div>
         </div>
         <Grid container spacing={16} direction="column" alignItems="center" justify="center">
-          <Grid item xs={12}>
+          <Grid item xs={12} style={{paddingTop: 40}}>
             <Grid container spacing={16} direction="row" alignItems="center" justify="center">
-              <Grid item xs={6}>
+              <Grid item xs={12} sm={6}>
                 <Button
                   fullWidth
                   style={{
@@ -95,7 +90,7 @@ class Home extends React.Component {
                   <ReceiveIcon style={{ marginLeft: "5px" }} />
                 </Button>
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={12} sm={6}>
                 <Button
                   fullWidth
                   style={{
