363b67096ea003ec5dc0b46b8afb6d9a559bc25d
Bo Henderson <bohende@gmail.com>
2019-02-20 11:56:06 +0530

    setup push on prod-mode build

--- ops/build.sh
+++ ops/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+registry=docker.io/connextproject
 image=connext_card:latest
 
 rm .env
@@ -8,4 +9,10 @@ cp ops/prod.env .env
 npm run build
 
 docker build --file ops/prod.dockerfile --tag $image .
+
+if [[ "$1" == "prod" ]]
+then
+  docker tag $image $registry/$image
+  docker push  $registry/$image
+fi
 
