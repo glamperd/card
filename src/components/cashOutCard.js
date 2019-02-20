ae7c10d9dfd548e29a297bdcc245499d2e8e5f7e
LayneHaber <layne.haber@gmail.com>
2019-02-19 21:45:51 -0800

    wire up cash out card

--- src/components/cashOutCard.js
+++ src/components/cashOutCard.js
@@ -11,7 +11,13 @@ import InputAdornment from "@material-ui/core/InputAdornment";
 import { BigNumber } from "bignumber.js";
 import Modal from "@material-ui/core/Modal";
 import QRScan from "./qrScan";
-import { withStyles, Grid } from "@material-ui/core";
+import { withStyles, Grid, Typography } from "@material-ui/core";
+import { convertChannelState } from "connext/dist/types"
+import { bigNumberify } from "ethers/utils";
+import { ethers } from "ethers";
+import { getDollarSubstring } from "../utils/getDollarSubstring";
+import { ConnextState } from "connext/dist/state/store";
+import { getAggregateChannelBalance } from "../utils/getAggregateChannelBalance";
 
 const styles = {
   icon: {
@@ -34,112 +40,115 @@ class CashOutCard extends Component {
         tokensToSell: "0",
         withdrawalTokenUser: "0",
         weiToSell: "0",
-        exchangeRate: "0.00",
-        recipient: "0x0..."
+        recipient: "0x0...",
       },
       addressError: null,
       balanceError: null,
-      scan: false
+      scan: false,
+      withdrawEth: true,
+      aggregateBalance: "0.00"
     };
   }
 
-  async updateWithdrawHandler(evt) {
-    this.setState({
-      displayVal: evt.target.value
-    });
-    var value = evt.target.value;
-    if (!this.state.checkedB) {
-      await this.setState(oldState => {
-        oldState.withdrawalVal.withdrawalWeiUser = value;
-        return oldState;
-      });
-    } else if (this.state.checkedB) {
-      await this.setState(oldState => {
-        oldState.withdrawalVal.tokensToSell = value;
-        return oldState;
-      });
+  async updateWithdrawalVals(withdrawEth) {
+    this.setState({ withdrawEth });
+
+    // set the state to contain the proper withdrawal args for
+    // eth or dai withdrawal
+    const { channelState, exchangeRate } = this.props
+    let { withdrawalVal } = this.state
+    if (withdrawEth) {
+      // withdraw all channel balance in eth
+      withdrawalVal = { ...withdrawalVal, exchangeRate, tokensToSell: channelState.balanceTokenUser, withdrawalWeiUser: channelState.balanceWeiUser, weiToSell: "0", withdrawalTokenUser: "0" }
+    } else { // handle withdrawing all channel balance in dai
+      withdrawalVal = { ...withdrawalVal, exchangeRate, tokensToSell: "0", withdrawalWeiUser: "0", weiToSell: channelState.balanceWeiUser, withdrawalTokenUser: channelState.balanceTokenUser }
     }
-    console.log(`Updated withdrawalVal: ${JSON.stringify(this.state.withdrawalVal, null, 2)}`);
+    
+
+    this.setState({ withdrawalVal })
+    
+    console.log(`Updated withdrawalVal: ${JSON.stringify(withdrawalVal, null, 2)}`);
+    return withdrawalVal
+  }
+
+  // examines if the display value should be updated
+  // when the component is mounting, or when the props change
+  async updateDisplayValue() {
+    const { channelState, exchangeRate } = this.props
+    if (!channelState || (channelState.balanceWeiUser == "0" && channelState.balanceTokenUser == "0")) {
+      this.setState({ aggregateBalance: "$0.00" })
+      return
+    }
+
+    const aggUSD = getAggregateChannelBalance(channelState, exchangeRate)
+
+    const substr = getDollarSubstring(aggUSD)
+    const aggregateBalance = '$' + substr[0] + '.' + substr[1].substr(0, 2)
+
+    this.setState({ aggregateBalance })
+  }
+
+  // update display value with the exchange rate/
+  // channel balance changes
+  async componentWillReceiveProps() {
+    await this.updateDisplayValue()
+  }
+
+  async componentDidMount() {
+    await this.updateDisplayValue()
   }
 
   async updateRecipientHandler(value) {
     this.setState({
-      recipientDisplayVal: value
+      recipientDisplayVal: value,
+      scan: false,
     });
     await this.setState(oldState => {
       oldState.withdrawalVal.recipient = value;
       return oldState;
     });
-    this.setState({ scan: false });
     console.log(`Updated recipient: ${JSON.stringify(this.state.withdrawalVal.recipient, null, 2)}`);
   }
 
-  async maxHandler() {
-    let withdrawalVal = {
-      ...this.state.withdrawalVal,
-      tokensToSell: this.props.channelState.balanceTokenUser,
-      withdrawalWeiUser: this.props.channelState.balanceWeiUser
-    };
-    let balance = new BigNumber(this.props.channelState.balanceTokenUser);
-    let tokenBalance = new BigNumber(this.props.channelState.balanceWeiUser);
-    let exchangeRate = new BigNumber(this.props.exchangeRate);
-    const tokenBalanceConverted = tokenBalance.dividedToIntegerBy(exchangeRate);
-    // const aggBalance = String(balance.plus(tokenBalanceConverted));
-    // console.log(aggBalance);
-
-    // i dont think we need the aggregate balance here, i think we can show both ETH and Token withdrawals separately
-    if (this.state.checkedB) {
-      this.setState({ displayVal: withdrawalVal.tokensToSell, withdrawalVal });
-    } else {
-      this.setState({ displayVal: withdrawalVal.withdrawalWeiUser, withdrawalVal });
-    }
-  }
-
-  async withdrawalHandler() {
-    let withdrawalVal = {
-      ...this.state.withdrawalVal,
-      exchangeRate: this.props.exchangeRate
-    };
-    console.log(`Withdrawing: ${JSON.stringify(this.state.withdrawalVal, null, 2)}`);
+  async withdrawalHandler(withdrawEth) {
+    const { channelState, connext, web3, exchangeRate } = this.props;
+    const withdrawalVal = await this.updateWithdrawalVals(withdrawEth)
     this.setState({ addressError: null, balanceError: null });
-    const { channelState, connext, web3 } = this.props;
-    // if (
-    //   Big(this.state.withdrawalVal.withdrawalWeiUser).isLessThanOrEqualTo(channelState.balanceWeiUser) &&
-    //   Big(this.state.withdrawalVal.tokensToSell).isLessThanOrEqualTo(channelState.balanceTokenUser)
-    // ) {
-    if (web3.utils.isAddress(this.state.withdrawalVal.recipient)) {
-      let withdrawalRes = await connext.withdraw(withdrawalVal);
-      console.log(`Withdrawal result: ${JSON.stringify(withdrawalRes, null, 2)}`);
-    } else {
-      this.setState({ addressError: "Please enter a valid address" });
+    // check for valid address
+    // let addressError = null
+    // let balanceError = null
+    if (!web3.utils.isAddress(withdrawalVal.recipient)) {
+      const addressError = `${withdrawalVal.recipient == "0x0..." ? "Must provide address." : withdrawalVal.recipient + " is not a valid address"}`
+      this.setState({ addressError })
+      return
     }
-    // } else {
-    //   this.setState({balanceError: "Insufficient balance in channel"})
-    // }
+    // check the input balance is under channel balance
+    // TODO: allow partial withdrawals?
+    console.log(`Withdrawing: ${JSON.stringify(withdrawalVal, null, 2)}`);
+    let withdrawalRes = await connext.withdraw(withdrawalVal);
+    console.log(`Withdrawal result: ${JSON.stringify(withdrawalRes, null, 2)}`);
   }
 
   render() {
-    const { classes } = this.props;
+    const { classes, exchangeRate, connextState } = this.props;
+    const { recipientDisplayVal, addressError, scan, withdrawEth, aggregateBalance } = this.state
     return (
       <Grid container spacing={24} direction="column" style={{ paddingLeft: 12, paddingRight: 12, textAlign: "center" }}>
         <Grid item xs={12}>
           <UnarchiveIcon className={classes.icon} />
         </Grid>
         <Grid item xs={12}>
-          <TextField
-            fullWidth
-            id="outlined-number"
-            label="Amount"
-            placeholder="$0.00"
-            required
-            value={this.state.amountToken}
-            onChange={evt => this.updatePaymentHandler(evt)}
-            type="number"
-            margin="normal"
-            variant="outlined"
-            helperText={this.state.balanceError}
-            error={this.state.balanceError != null}
-          />
+          <Grid container direction="row" justify="center" alignItems="center">
+          <Typography variant="h2">
+            <span>{aggregateBalance}</span>
+          </Typography>
+          <img src={withdrawEth ? EthIcon : DaiIcon} style={{ width: "25px", height: "25px", marginLeft: "5px" }}></img>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="caption">
+            <span>{'ETH price: $' + exchangeRate}</span>
+          </Typography>
         </Grid>
         <Grid item xs={12}>
           <TextField
@@ -147,13 +156,13 @@ class CashOutCard extends Component {
             id="outlined-with-placeholder"
             label="Address"
             placeholder="0x0..."
-            value={this.state.recipientDisplayVal}
+            value={recipientDisplayVal}
             onChange={evt => this.updateRecipientHandler(evt.target.value)}
             margin="normal"
             variant="outlined"
             required
-            helperText={this.state.addressError}
-            error={this.state.addressError != null}
+            helperText={addressError}
+            error={addressError != null}
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
@@ -167,30 +176,19 @@ class CashOutCard extends Component {
             }}
           />
         </Grid>
-        <Modal id="qrscan" open={this.state.scan} onClose={() => this.setState({ scan: false })} style={{ width: "full", height: "full" }}>
+        <Modal id="qrscan" open={scan} onClose={() => this.setState({ scan: false })} style={{ width: "full", height: "full" }}>
           <QRScan handleResult={this.updateRecipientHandler.bind(this)} />
         </Modal>
         <Grid item xs={12}>
-          <TextField
-            fullWidth
-            id="outlined-with-placeholder"
-            label="Exchange Rate"
-            variant="outlined"
-            InputProps={{ "text-align": "center" }}
-            disabled
-            value={this.props.exchangeRate}
-          />
-        </Grid>
-        <Grid item xs={12}>
           <Grid container spacing={8} direction="row" alignItems="center" justify="center">
             <Grid item xs={6}>
-              <Button className={classes.button} fullWidth>
+              <Button className={classes.button} fullWidth onClick={() => this.withdrawalHandler(true)} disabled={!connextState || !connextState.runtime.canWithdraw}>
                 Cash Out Eth
                 <img src={EthIcon} style={{ width: "15px", height: "15px", marginLeft: "5px" }} />
               </Button>
             </Grid>
             <Grid item xs={6}>
-              <Button className={classes.button} fullWidth>
+              <Button className={classes.button} fullWidth onClick={() => this.withdrawalHandler(false)} disabled={!connextState || !connextState.runtime.canWithdraw}>
                 Cash Out Dai
                 <img src={DaiIcon} style={{ width: "15px", height: "15px", marginLeft: "5px" }} />
               </Button>
