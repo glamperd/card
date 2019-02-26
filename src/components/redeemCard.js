291e63bcab311e5877cceb017242474e5d3311d7
LayneHaber <layne.haber@gmail.com>
2019-02-25 18:38:18 -0700

    add basic layout

--- src/components/redeemCard.js
+++ src/components/redeemCard.js
@@ -1,9 +1,15 @@
-import { DepositCard } from "./depositCard";
 import { withStyles, Button } from "@material-ui/core";
+import ReceiveIcon from "@material-ui/icons/SaveAlt";
 import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Tooltip from "@material-ui/core/Tooltip";
 import DepositIcon from "@material-ui/icons/AttachMoney";
+import QRGenerate from "./qrGenerate";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
+
+const queryString = require("query-string");
 
 const styles = theme => ({
   icon: {
@@ -18,26 +24,80 @@ class RedeemCard extends Component {
 
     this.state = {
       secret: null,
-      redeemer: null
+      confirmation: false,
     };
   }
 
+  async componentWillMount() {
+    const { location } = this.props;
+    const query = queryString.parse(location.search);
+    console.log("query", query);
+    if (query.secret) {
+      this.setState({ secret: query.secret, confirmation: true });
+    }
+  }
+
+  generateQrUrl(secret) {
+    const { publicUrl } = this.props;
+    const url = `${publicUrl}/redeem?secret=${secret ? secret : ""}`;
+    return url;
+  }
+
   render() {
-    const { classes, address } = this.props;
+    const { secret } = this.state;
+    const { classes } = this.props;
+    const url = this.generateQrUrl(secret);
     return (
-      <Button
-        fullWidth
-        color="primary"
+      <Grid
+        container
+        spacing={24}
+        direction="column"
         style={{
-          background: "#FFF",
-          border: "1px solid #7289da",
-          color: "#7289da"
+          paddingLeft: 12,
+          paddingRight: 12,
+          paddingTop: "10%",
+          paddingBottom: "10%",
+          textAlign: "center",
+          justifyContent: "center"
         }}
-        size="large"
       >
-      Import from Backup
-      </Button>
-      
+        <Grid item xs={12}>
+          <ReceiveIcon className={classes.icon} />
+        </Grid>
+        <Grid item xs={12}>
+          <QRGenerate value={url} />
+        </Grid>
+        <Grid item xs={12}>
+          <CopyToClipboard text={url}>
+            <Button variant="outlined" fullWidth>
+              <Typography noWrap variant="body1">
+                <Tooltip
+                  disableFocusListener
+                  disableTouchListener
+                  title="Click to Copy"
+                >
+                  <span>{url}</span>
+                </Tooltip>
+              </Typography>
+            </Button>
+          </CopyToClipboard>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            variant="outlined"
+            style={{
+              background: "#FFF",
+              border: "1px solid #F22424",
+              color: "#F22424",
+              width: "15%"
+            }}
+            size="medium"
+            onClick={() => this.props.history.push("/")}
+          >
+            Back
+          </Button>
+        </Grid>
+      </Grid>
     );
   }
 }
