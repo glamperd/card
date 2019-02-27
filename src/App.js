9d9318acb6b586d32293088e144272851aa2ef8d
LayneHaber <layne.haber@gmail.com>
2019-02-27 12:09:01 -0800

    update wei calculation, track in local storage, still issues with web3 balance

--- src/App.js
+++ src/App.js
@@ -137,7 +137,10 @@ class App extends React.Component {
 
   async componentDidMount() {
     // Set up state
-    const mnemonic = localStorage.getItem("mnemonic");
+    const mnemonic = localStorage.getItem("mnemonic")
+    // on mount, check if you need to refund by removing maxBalance
+    localStorage.removeItem("refunding")
+    localStorage.removeItem("maxBalanceAfterRefund")
     let rpc = localStorage.getItem("rpc");
     // TODO: better way to set default provider
     // if it doesnt exist in storage
@@ -164,7 +167,6 @@ class App extends React.Component {
       await this.setTokenContract();
       await this.authorizeHandler();
 
-      console.log(this.state.connext);
       await this.pollConnextState();
       await this.poller();
     } else {
@@ -186,6 +188,9 @@ class App extends React.Component {
     // called from settingsCard when a new RPC URL is connected
     // will create a new custom web3 and reinstantiate connext
     localStorage.setItem("rpc", rpc);
+    // update refunding variable on rpc switch
+    localStorage.removeItem("maxBalanceAfterRefund")
+    localStorage.removeItem("refunding")
     await this.setWeb3(rpc);
     await this.setConnext();
     await this.setTokenContract();
@@ -305,8 +310,26 @@ class App extends React.Component {
   }
 
   async autoDeposit() {
-    const { address, tokenContract, customWeb3, connextState, tokenAddress, exchangeRate, channelState, connext } = this.state;
+    const { address, tokenContract, customWeb3, connextState, tokenAddress, exchangeRate, channelState } = this.state;
     const balance = await customWeb3.eth.getBalance(address);
+    console.log('balance wallet:', balance.toString())
+
+    const refunding = localStorage.getItem('refunding')
+    if (refunding && refunding == 'true') {
+      return
+    }
+
+    const maxBalanceAfterRefund = localStorage.getItem("maxBalanceAfterRefund")
+    console.log('maxBalanceAfterRefund:', maxBalanceAfterRefund)
+    if (maxBalanceAfterRefund && new BigNumber(balance).gte(new BigNumber(maxBalanceAfterRefund))) {
+      console.log('wallet balance hasnt changed since submitting tx, returning')
+      return
+    } else {
+      // tx has been submitted, delete the maxWalletBalance from storage
+      localStorage.removeItem("refunding")
+      localStorage.removeItem("maxBalanceAfterRefund")
+    }
+
     let tokenBalance = "0";
     try {
       tokenBalance = await tokenContract.methods.balanceOf(address).call();
@@ -321,7 +344,10 @@ class App extends React.Component {
     if (balance !== "0" || tokenBalance !== "0") {
       if (eth.utils.bigNumberify(balance).lte(DEPOSIT_MINIMUM_WEI)) {
         // don't autodeposit anything under the threshold
-        console.log('balance below min')
+        console.log('below min threshold')
+        // update the refunding variable before returning
+        localStorage.removeItem("refunding")
+        localStorage.removeItem("maxBalanceAfterRefund")
         return;
       }
       // only proceed with deposit request if you can deposit
@@ -332,14 +358,17 @@ class App extends React.Component {
 
       // if you already have the maximum balance tokens hub will exchange
       // do not deposit any more eth to be swapped
-      if (eth.utils.bigNumberify(channelState.balanceTokenUser).gte(HUB_EXCHANGE_CEILING)) {
+      // TODO: figure out rounding error, for now ignore if you have 6.8
+
+      if (eth.utils.bigNumberify(channelState.balanceTokenUser).gte(eth.utils.parseEther("6.88"))) {
         console.log('Channel state token balance at max, refunding browser balance')
-        // refund any wei that is in the browser wallet and exit
+        // refund any wei that is in the browser wallet 
+        // above the minimum
         const refundWei = BigNumber.max(
           new BigNumber(balance).minus(DEPOSIT_MINIMUM_WEI),
           0
         )
-        await this.returnWeiDeposit(refundWei.toString())
+        await this.returnWei(refundWei.toString())
         return
       }
 
@@ -358,13 +387,19 @@ class App extends React.Component {
 
       // if amount to deposit into channel is over the channel max
       // then return excess deposit to the sending account
-      const weiToReturn = this.calculateWeiAboveMax(channelDeposit.amountWei, exchangeRate)
+      const weiToReturn = this.calculateWeiToRefund(channelDeposit.amountWei, exchangeRate)
       console.log('wei to return to user:', weiToReturn.toString())
 
       // return wei to sender
-      const weiDeposit = new BigNumber(channelDeposit.amountWei)
-      await this.returnWeiDeposit(weiToReturn.toString())
-      channelDeposit.amountWei = weiDeposit.minus(weiToReturn).toString()
+      if (!weiToReturn.isZero()) {
+        await this.returnWei(weiToReturn.toString())
+        return
+      }
+      // update channel deposit
+      const weiDeposit = new BigNumber(channelDeposit.amountWei).minus(
+        weiToReturn
+      )
+      channelDeposit.amountWei = weiDeposit.toString()
 
       console.log(`Depositing: ${JSON.stringify(channelDeposit, null, 2)}`);
       let depositRes = await this.state.connext.deposit(channelDeposit);
@@ -372,8 +407,9 @@ class App extends React.Component {
     }
   }
 
-  async returnWeiDeposit(wei) {
+  async returnWei(wei) {
     const { address, customWeb3 } = this.state;
+    localStorage.setItem('refunding', true)
 
     if (!customWeb3) {
       return
@@ -396,57 +432,59 @@ class App extends React.Component {
     }
     // sort by nonce and take latest senders address and
     // return wei to the senders address
-    const filteredTxs = txs.filter(t => t.to.toLowerCase() == address.toLowerCase())
+    const filteredTxs = txs.filter(t => t.to && t.to.toLowerCase() == address.toLowerCase())
     const mostRecent = (filteredTxs.sort((a, b) => b.nonce - a.nonce))[0]
     if (!mostRecent) {
       console.log('Browser wallet overfunded, but couldnt find most recent tx in last 100 blocks.')
       return
     }
-    console.log(`Refunding ${wei} to ${mostRecent.from}`)
+    console.log(`Refunding ${wei} to ${mostRecent.from} from ${address}`)
+    const origBalance = new BigNumber(await customWeb3.eth.getBalance(address))
+    const newMax = origBalance.minus(new BigNumber(wei))
+    console.log('maxBalanceAfterRefund:', newMax.toString())
 
     try {
-      const tx = await customWeb3.eth.sendTransaction({
-        from: mostRecent.to,
+      const res = await customWeb3.eth.sendTransaction({
+        from: address,
         to: mostRecent.from,
         value: wei,
       })
-      console.log(tx)
+      const tx = await customWeb3.eth.getTransaction(res.transactionHash)
+      console.log('tx', tx)
+      // calculate expected balance after transaction and set in local
+      // storage. once the tx is submitted, the wallet balance should
+      // always be lower than the expected balance, because of added
+      // gas costs
+      localStorage.setItem('maxBalanceAfterRefund', newMax.toString())
     } catch (e) {
       console.log('Error with transaction:', e.message)
     }
+    localStorage.removeItem('refunding')
   }
 
   // returns a BigNumber
-  calculateWeiAboveMax(wei, exchangeRate) {
-    // calculate the autoswap amount detected
-    const args = {
+  calculateWeiToRefund(wei, exchangeRate) {
+    // calculate the max wei the hub is willing to exchange
+    const maxWei = {
       exchangeRate,
       seller: "user",
-      tokensToSell: "0",
-      weiToSell: wei,
-    }
-    const { tokensReceived } = calculateExchange(convertExchange('bn', args))
-    
-    const tokensToReturn = BigNumber.max(
-      new BigNumber(tokensReceived.toString()).minus(HUB_EXCHANGE_CEILING),
-      0
-    )
-    if (tokensToReturn.isZero()) {
-      return tokensToReturn
-    }
-
-    // use connext functions to calculate the appropriate equicalent
-    // amount of wei to be returned to user
-    const updatedArgs = {
-      exchangeRate,
-      seller: "user",
-      tokensToSell: tokensToReturn.toString(),
+      tokensToSell: HUB_EXCHANGE_CEILING.toString(),
       weiToSell: "0"
     }
-    // wei received is amount wei that should be returned to user
-    const { weiReceived } = calculateExchange(convertExchange('bn', updatedArgs))
 
-    return new BigNumber(weiReceived.toString())
+    // see notes in src about tokensSold for "calculateExchange"
+    const { weiReceived, tokensSold } = calculateExchange(convertExchange('bn', maxWei))
+
+    console.log('tokens sold by hub', tokensSold.toString())
+
+    // calculate the wei to refund to user
+    let weiToRefund = new BigNumber(wei).minus(new BigNumber(weiReceived.toString()))
+    
+    if (weiToRefund.isNegative()) {
+      return new BigNumber(0)
+    }
+
+    return weiToRefund
   }
 
   async autoSwap() {
