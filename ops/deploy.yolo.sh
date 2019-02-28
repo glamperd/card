b687c2e3b43f89b89d10884498e4ec543412f828
Bo Henderson <bohende@gmail.com>
2019-02-28 12:18:27 +0530

    yolo deploy: ensure the remote server is deploying to the proper domain name

--- ops/deploy.yolo.sh
+++ ops/deploy.yolo.sh
@@ -16,11 +16,16 @@ if [[ "$?" != "0" ]]
 then echo "Make sure you're logged into docker & have push permissions: docker login"
 fi
 
+# Make sure the prod server has the card repo available
+ssh -i $ssh_key $user@$prod_server "bash -c 'git clone https://github.com/ConnextProject/card.git 2> /dev/null || true'"
+
+# Make sure the prod server's repo is up to date with master
+ssh -i $ssh_key $user@$prod_server "bash -c 'cd card && git fetch && git reset --hard origin/master'"
+
 echo;echo
 echo "Preparing to re-deploy the card app to $prod_server. Without running any tests. Good luck."
 echo;echo
 sleep 2 # Give the user one last chance to ctrl-c before we pull the trigger
 
-ssh -i $ssh_key $user@$prod_server "bash -c 'git clone https://github.com/ConnextProject/card.git 2> /dev/null || true'"
-
-ssh -i $ssh_key $user@$prod_server "bash -c 'cd card && git fetch && git reset --hard origin/master && bash ops/restart.sh prod'"
+# Deploy!
+ssh -i $ssh_key $user@$prod_server "bash -c 'cd card && DOMAINNAME=$prod_server bash ops/restart.sh prod'"
