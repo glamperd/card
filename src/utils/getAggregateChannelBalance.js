9d8420801fbaaa44e7370757562a5188d2014843
LayneHaber <layne.haber@gmail.com>
2019-03-07 18:41:27 -0800

    should use convertable, returns dollar string

--- src/utils/getAggregateChannelBalance.js
+++ src/utils/getAggregateChannelBalance.js
@@ -1,8 +1,20 @@
-import BigNumber from "bignumber.js";
+import CurrencyConvertable from "connext/dist/lib/currency/CurrencyConvertable";
+import { CurrencyType } from "connext/dist/state/ConnextState/CurrencyTypes";
+import getExchangeRates from "connext/dist/lib/getExchangeRates";
+import Currency from "connext/dist/lib/currency/Currency";
 
-export function getAggregateChannelBalance(channelState, exchangeRate) {
-  const wei = new BigNumber(channelState.balanceWeiUser);
-  const token = new BigNumber(channelState.balanceTokenUser);
-  const aggUSD = token.plus(wei.multipliedBy(exchangeRate));
-  return aggUSD.toFixed();
+export function getChannelBalanceInUSD(channelState, connextState, onlyTokens = true) {
+  if (onlyTokens) {
+    return Currency.USD(channelState.balanceTokenUser).format({})
+  }
+
+  const convertableTokens = new CurrencyConvertable(CurrencyType.BEI, channelState.balanceTokenUser, () => getExchangeRates(connextState))
+
+  const convertableWei = new CurrencyConvertable(CurrencyType.WEI, channelState.balanceWeiUser, () => getExchangeRates(connextState))
+
+  const totalBalance = Currency.USD(
+    convertableTokens.toBEI().amountBigNumber.plus(convertableWei.toBEI)
+  ).format({})
+
+  return totalBalance
 }
