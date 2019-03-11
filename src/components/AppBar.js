4bc3401c8a9fd200cc11eeca0230298064ff52bf
arjunbhuptani <arjunbhuptani@gmail.com>
2019-03-11 20:24:36 +0400

    4px radius on blocky

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -43,12 +43,12 @@ const AppBarComponent = props => (
                   : noAddrBlocky
               }
               alt=""
-              style={{ width: "40px", height: "40px" }}
+              style={{ width: "40px", height: "40px", borderRadius: "4px" }}
             />
             <Typography
               variant="body2"
               noWrap
-              style={{ width: "75px", color: "#c1c6ce", marginLeft: "3px" }}
+              style={{ width: "75px", color: "#c1c6ce", marginLeft: "0.5em" }}
             >
               <span>{props.address}</span>
             </Typography>
