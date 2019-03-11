ec7f66d1175f26d8de87676fe63b09ff59c644bf
Bo Henderson <bohende@gmail.com>
2019-03-11 15:28:58 +0530

    init cypress files

---
+++ cypress/integration/deposit.js
@@ -0,0 +1,6 @@
+describe('Deposit', function() {
+  it('Displays the deposit address', function() {
+    cy.visit('https://localhost')
+    cy.contains('img')
+  })
+})
---
+++ cypress/integration/smoke.js
@@ -0,0 +1,5 @@
+describe('Smoke test', function() {
+  it('Loads the page', function() {
+    cy.visit('https://localhost')
+  })
+})
