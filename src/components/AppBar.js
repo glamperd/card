fcffb6225d8061021356bde2a7dacd93f66c757e
LayneHaber <layne.haber@gmail.com>
2019-03-12 14:03:02 -0700

    fix sizing

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -14,68 +14,68 @@ import { Link } from "react-router-dom";
 const noAddrBlocky = require("../assets/noAddress.png");
 
 const AppBarComponent = props => (
-  <AppBar
-    position="sticky"
-    color="secondary"
-    elevation={0}
-    style={{ paddingTop: "2%" }}
-  >
-    <Toolbar>
-      <Grid
-        container
-        spacing={16}
-        direction="row"
-        justify="space-between"
-        alignItems="center"
-        style={{ textAlign: "center" }}
-      >
-        <Grid item xs={3}>
-          <IconButton
-            color="inherit"
-            variant="contained"
-            component={Link}
-            to="/deposit"
+  <Grid>
+    <Grid container spacing={16}>
+      <AppBar position="sticky" color="secondary" elevation={0} style={{ paddingTop: "2%" }}>
+        <Toolbar>
+          <Grid
+            container
+            spacing={16}
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+            style={{ textAlign: "center" }}
           >
-            <img
-              src={
-                props.address
-                  ? blockies.createDataURL({ seed: props.address })
-                  : noAddrBlocky
-              }
-              alt=""
-              style={{ width: "40px", height: "40px", borderRadius: "4px" }}
-            />
-            <Typography
-              variant="body2"
-              noWrap
-              style={{ width: "75px", color: "#c1c6ce", marginLeft: "0.5em" }}
-            >
-              <span>{props.address}</span>
-            </Typography>
-          </IconButton>
-        </Grid>
-        <Grid item xs={5}>
-          <Button
-            size="small"
-            variant="outlined"
-            style={{
-              color: "#c1c6ce",
-              borderColor: "#c1c6ce",
-              fontSize: "small"
-            }}
-            component={Link}
-            to="/settings"
-          >
-            {localStorage.getItem("rpc-prod")}
-            <SettingIcon style={{ marginLeft: "3px" }} />
-          </Button>
-          {/* <IconButton color="inherit" variant="contained" component={Link} to="/settings">
-
-          </IconButton> */}
-        </Grid>
-      </Grid>
-    </Toolbar>
-  </AppBar>
+            <Grid item xs={3}>
+              <IconButton
+                color="inherit"
+                variant="contained"
+                component={Link}
+                to="/deposit"
+              >
+                <img
+                  src={
+                    props.address
+                      ? blockies.createDataURL({ seed: props.address })
+                      : noAddrBlocky
+                  }
+                  alt=""
+                  style={{ width: "40px", height: "40px", borderRadius: "4px" }}
+                />
+                <Typography
+                  variant="body2"
+                  noWrap
+                  style={{
+                    width: "75px",
+                    color: "#c1c6ce",
+                    marginLeft: "0.5em"
+                  }}
+                >
+                  <span>{props.address}</span>
+                </Typography>
+              </IconButton>
+            </Grid>
+            <Grid item xs={5}>
+              <Button
+                size="small"
+                variant="outlined"
+                style={{
+                  color: "#c1c6ce",
+                  borderColor: "#c1c6ce",
+                  fontSize: "small"
+                }}
+                component={Link}
+                to="/settings"
+              >
+                {localStorage.getItem("rpc-prod")}
+                <SettingIcon style={{ marginLeft: "3px" }} />
+              </Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </Grid>
+  </Grid>
 );
 
 export default AppBarComponent;
