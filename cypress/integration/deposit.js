6f30f48437093b7ab5a37c0485de342e0578e052
Bo Henderson <bohende@gmail.com>
2019-03-12 18:24:55 +0530

    fix cypress promises

--- cypress/integration/deposit.js
+++ cypress/integration/deposit.js
@@ -4,8 +4,7 @@ const wallet = eth.Wallet.fromMnemonic(Cypress.env('mnemonic')).connect(provider
 
 describe('Deposit', () => {
   it('Displays loading message while connext gets started', () => {
-    cy.visit('http://localhost/deposit')
-    // Wait for connext controllers to all start up
+    cy.visit('http://localhost')
     cy.get('span#message-id').should('not.exist')
   })
 
@@ -13,25 +12,24 @@ describe('Deposit', () => {
     cy.visit('http://localhost')
     cy.get('span#message-id').should('not.exist')
     cy.get('a[href="/deposit"]').click()
-    // NOTE: Copy/paste in cypress doesn't work yet but will be added soon:
-    // https://github.com/cypress-io/cypress/issues/311
     cy.get('button').contains(/0[Xx]/).invoke('text').should('match', /0[xX][0-9a-zA-Z]{40}/)
   })
 
-  it.only('Accepts a deposit to displayed address', () => {
+  it('Accepts a deposit to displayed address', () => {
     cy.visit('http://localhost')
     cy.get('span#message-id').should('not.exist')
     cy.get('a[href="/deposit"]').click()
-    cy.get('button').contains(/0[Xx]/).invoke('text').then(async (address) => {
-
+    cy.get('button').contains(/0[Xx]/).invoke('text').then(address => {
       cy.get('button').contains("Back").click()
-      console.log(`Sending funds to: ${address}`)
-      const tx = await wallet.sendTransaction({ to: address, value: eth.utils.parseEther('0.03') })
-      console.log(`Waiting for tx to get mined: ${tx.hash}`)
-      await wallet.provider.waitForTransaction(tx.hash)
-      console.log(`Mined!`)
-      cy.get('h3').children('span').contains('00').should('not.exist')
-
+      wallet.sendTransaction({
+        to: address,
+        value: eth.utils.parseEther('0.03')
+      }).then(() => {
+        wallet.provider.waitForTransaction(tx.hash).then(() => {
+          cy.get('h3').children('span').contains('00').should('not.exist')
+        })
+      })
     })
+
   })
 })
--- cypress/integration/smoke.js
+++
@@ -1,5 +0,0 @@
-describe('Smoke test', function() {
-  it('Loads the page', function() {
-    cy.visit('http://localhost')
-  })
-})
