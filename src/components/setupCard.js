360466670fc20259cfed54f2690fa8f2bad99f54
LayneHaber <layne.haber@gmail.com>
2019-03-13 21:55:53 -0700

    clarify messaging

--- src/components/setupCard.js
+++ src/components/setupCard.js
@@ -71,10 +71,16 @@ const screens = (classes, minEth, minDai, maxEth, maxDai) => [
   {
     title: "Deposit Boundaries",
     message: `The card needs a minimum deposit of ${
-      minEth || "?.??"} eth (${
-      minDai || "?.??"} dai) to cover the gas costs of getting setup. Cards only accept deposits of ${
-      maxEth || "?.??"} eth (${
-      maxDai || "?.??"} dai) or less, with any excess getting refunded.`
+      minEth || "?.??"} eth ($${
+      minDai || "?.??"}) to cover the gas costs of getting setup. Cards only accept deposits of ${
+      maxEth || "?.??"} eth ($${
+      maxDai || "?.??"}) or less, with any excess eth getting refunded.`
+  },
+  {
+    title: "Depositing Tokens",
+    message: `If you want to deposit dai directly, there are no deposit maximums enforced! Just make sure to send at least ${
+      minEth || "?.??"} eth ($${
+      minDai || "?.??"}) for gas to your new wallet.`
   }
 ];
 
