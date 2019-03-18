27420a8c6d79ca4e81506e1151590d4da39b240c
Bo Henderson <bohende@gmail.com>
2019-03-18 16:26:08 +0530

    e2e: init tests for burning & restoring a card

--- cypress/integration/common.js
+++ cypress/integration/common.js
@@ -3,7 +3,6 @@
 // Define exportable helper functions
 
 const closeIntroModal = () => {
-  cy.visit(Cypress.env('publicUrl'))
   // Click through intro modal
   cy.get('button').contains(/next/i).click()
   cy.get('button').contains(/next/i).click()
@@ -17,14 +16,48 @@ const closeIntroModal = () => {
   cy.get('span').contains('Starting').should('not.exist')
 }
 
-const restoreMnemonic = () => { }
-
 ////////////////////////////////////////
 // Run tests
 
 describe('Common', () => {
   it('Should display an intro modal that can be closed', () => {
+    cy.visit(Cypress.env('publicUrl'))
     closeIntroModal()
+  })
+
+  it('Should display our backup mnemonic', () => {
+    cy.visit(Cypress.env('publicUrl'))
+    closeIntroModal()
+    cy.get('a[href="/settings"]').click()
+    cy.get('button').contains(/([a-zA-Z]{3,}\s?){12}/).should('not.exist')
+    cy.get('button').contains(/backup phrase/i).click()
+    cy.get('button').contains(/([a-zA-Z]{3,}\s?){12}/).should('exist')
+  })
+
+  it.only('Should restore the same card from mnemonic after burning', () => {
+    cy.visit(Cypress.env('publicUrl'))
+    closeIntroModal()
+    cy.get('a[href="/deposit"]').click()
+    cy.get('button').contains(/0[Xx]/).invoke('text').then(address => {
+      cy.log(`address=${address}`)
+      cy.get('a[href="/settings"]').click()
+      cy.get('button').contains(/([a-zA-Z]{3,}\s?){12}/).should('not.exist')
+      cy.get('button').contains(/backup phrase/i).click()
+      cy.get('button').contains(/([a-zA-Z]{3,}\s?){12}/).invoke('text').then(mnemonic => {
+        cy.log(`mnemonic=${mnemonic}`)
+        cy.get('button').contains(/burn card/i).click()
+        cy.get('button').contains(/burn$/i).click()
+        cy.get('p').contains(/burning/i).should('not.exist')
+        cy.reload()
+        closeIntroModal()
+        cy.get('a[href="/settings"]').click()
+        cy.get('button').contains(/import/i).click()
+        cy.get('input[type="text"]').type(mnemonic)
+        cy.get('button').find('svg').click()
+        cy.get('a[href="/deposit"]').click()
+        cy.get('button').contains(/0[Xx]/).invoke('text').should('eql', address)
+      })
+    })
   })
 })
 
--- cypress/integration/deposit.js
+++ cypress/integration/deposit.js
@@ -8,6 +8,7 @@ const wallet = eth.Wallet.fromMnemonic(Cypress.env('mnemonic')).connect(provider
 // Define exportable helper functions
 
 const deposit = (value) => {
+  cy.get('a[href="/deposit"]').click()
   cy.get('button').contains(/0[Xx]/).invoke('text').then(address => {
     cy.log(`Sending 0.03 eth from ${wallet.address} to ${address}`)
     return wallet.sendTransaction({
@@ -17,7 +18,6 @@ const deposit = (value) => {
       cy.get('button').contains("Back").click()
       cy.log(`Transaction sent, waiting for it to get mined..`)
       return wallet.provider.waitForTransaction(tx.hash).then(() => {
-        cy.wait(1000)
         cy.log(`Transaction mined, waiting for the deposit to be accepted..`)
         cy.get('span').contains('Processing').should('exist')
         cy.get('h3').children('span').contains('00').should('not.exist')
@@ -32,12 +32,15 @@ const deposit = (value) => {
 
 describe('Deposit', () => {
   beforeEach(() => {
+    cy.visit(Cypress.env('publicUrl'))
     closeIntroModal()
-    cy.get('a[href="/deposit"]').click()
   })
 
   it('Displays a valid deposit address', () => {
+    cy.get('a[href="/deposit"]').click()
     cy.get('button').contains(/0[Xx]/).invoke('text').should('match', /0[xX][0-9a-zA-Z]{40}/)
+    // TODO: test for copy snackbar once cypress fixes: github.com/cypress-io/cypress/issues/2739
+    //cy.get('button').contains(/0[Xx]/).click();cy.get('span').contains('Copied').should('exist')
   })
 
   it('Accepts a deposit to displayed address', () => {
