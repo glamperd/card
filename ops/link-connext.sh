6bf9387f8264fc0f7f5d0e0768ce982d6be27476
Bo Henderson <bohende@gmail.com>
2019-04-16 21:54:40 +0530

    fix ops/link-connext.sh

--- ops/link-connext.sh
+++ ops/link-connext.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 external_client="../indra/modules/client"
 
@@ -12,14 +13,21 @@ rm -rf connext
 echo "cp -rf ../indra/modules/client connext"
 cp -rf ../indra/modules/client connext
 
-echo "rm -rf node_modules/connext"
-rm -rf node_modules/connext
-
-echo "ln -s ../connext node_modules/connext"
-ln -s ../connext node_modules/connext
-
 echo "rebuilding the client..."
-cd connext && npm run build
+cd connext
+npm run build
+cd ..
+
+echo "rm -rf node_modules/connext/dist"
+rm -rf node_modules/connext/dist
+
+echo "cp -r connext/dist node_modules/connext/dist"
+cp -r connext/dist node_modules/connext/dist
 
 echo "Done!"
 
+# NOTE: You might want to replace line 80 of:
+# node_modules/react-scripts/config/webpackDevServer.config.js
+# with:
+# ignored: [/node_modules\/(?!connext)/,/connext\/node_modules/], poll: 1000
+# (Thanks: https://stackoverflow.com/a/44166532)
